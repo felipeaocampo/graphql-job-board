8d069dc08955f1faa62c1c9b24fedd8a010314ef
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-03-21 01:55:00 -0400

    refactors graphql client on the front end

--- client/src/components/CompanyDetail.js
+++ client/src/components/CompanyDetail.js
@@ -1,18 +1,28 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { companies } from '../fake-data';
+import { getCompany } from '../graphql/queries';
+import JobList from './JobList';
+
+const initialCompany = {
+  name: '',
+  description: '',
+  jobs: [],
+};
 
 function CompanyDetail() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState(initialCompany);
 
-  const company = companies.find((company) => company.id === companyId);
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
+
   return (
     <div>
-      <h1 className="title">
-        {company.name}
-      </h1>
-      <div className="box">
-        {company.description}
-      </div>
+      <h1 className="title">{company.name}</h1>
+      <div className="box">{company.description}</div>
+      <h5 className="title is-5">Jobs at {company.name}</h5>
+      <JobList jobs={company.jobs} />
     </div>
   );
 }
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,51 @@
+import { Job, Company } from './db.js';
+
+function rejectIf(condition) {
+  if (condition) {
+    throw new Error(`Unauthorized`);
+  }
+}
+
 export const resolvers = {
   Query: {
-    greeting: () => `Hello world`,
+    jobs: () => Job.findAll(),
+    job: (root, { id }) => Job.findById(id),
+    company: (root, { id }) => Company.findById(id),
+  },
+  Mutation: {
+    createJob: (root, { input }, { user }) => {
+      rejectIf(!user);
+
+      user.companyId;
+      return Job.create({ ...input, companyId: user.companyId });
+    },
+    deleteJob: async (root, { id }, { user }) => {
+      rejectIf(!user);
+
+      const job = await Job.findById(id);
+
+      if (job.companyId === user.companyId) {
+        return Job.delete(id);
+      } else {
+        throw new Error(`Cannot delete another company's job!`);
+      }
+    },
+    updateJob: async (root, { input }, { user }) => {
+      rejectIf(!user);
+
+      const job = await Job.findById(input.id);
+
+      rejectIf(user.companyId !== job.companyId);
+
+      return Job.update({ ...input, companyId: user.companyId });
+    },
+  },
+
+  Company: {
+    jobs: ({ id }) => Job.findAll((job) => job.companyId === id),
+  },
+
+  Job: {
+    company: ({ companyId }) => Company.findById(companyId),
   },
 };
